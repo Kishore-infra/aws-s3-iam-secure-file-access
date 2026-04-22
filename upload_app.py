3f5c5fdbfa7d7d419200c3908d8f155a492f2a67
Ubuntu <ubuntu@ip-172-31-25-217.ec2.internal>
2026-04-22 12:50:54 +0000

    Dynamic file access with validation

--- upload_app.py
+++ upload_app.py
@@ -1,24 +1,23 @@
-from flask import Flask, redirect
+from flask import Flask, request, redirect
 import boto3
 
 app = Flask(__name__)
 
 s3 = boto3.client('s3')
 
-@app.route("/")
-def home():
-    return "Flask app is Running Successfully"
+@app.route("/download")
+def download():
+    filename = request.args.get("file")
 
-@app.route("/get-url")
-def get_presigned_url():
     url = s3.generate_presigned_url(
         'get_object',
         Params={
             'Bucket': 'kishore-upload-project-01',
-            'Key': 'kishore resume.pdf'
+            'Key': filename
         },
         ExpiresIn=3600
     )
+
     return redirect(url)
 
 if __name__ == "__main__":
