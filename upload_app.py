62c4000cab8a269338721df14712a83918627f9e
Ubuntu <ubuntu@ip-172-31-25-217.ec2.internal>
2026-04-22 12:23:00 +0000

    generate presigned URl with JSON response

---
+++ upload_app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import boto3
+
+app = Flask(__name__)
+
+s3 = boto3.client('s3')
+
+@app.route("/")
+def home():
+    return "Flask app is Running Successfully"
+
+@app.route("/get-url")
+def get_presigned_url():
+    url = s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': 'kishore-upload-project-01',
+            'Key': 'kishore resume.pdf'
+        },
+        ExpiresIn=3600
+    )
+    return jsonify({"url": url})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
